fc7821e7f2604c0c0fb2d1d902d05fcc09cea7d5
Matteo Emanuele M <superma77@gmail.com>
2018-05-07 09:04:34 +0200

    step-4

--- src/main/webapp/protected/databaseBackups/develop/portDataSource/guifragment.sql
+++ src/main/webapp/protected/databaseBackups/develop/portDataSource/guifragment.sql
@@ -1999,3 +1999,23 @@ INSERT INTO guifragment (code,widgettypecode,plugincode,gui,defaultgui,locked) V
 </a>
 <div id="awcc1514712565101" class="aw-widget-current"  data-locationkey="" data-unit="f" data-language="en-us" data-useip="true" data-uid="awcc1514712565101"></div>
 <script type="text/javascript" src="https://oap.accuweather.com/launch.js"></script>',NULL,0);
+
+INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, locked) VALUES ('random_number_ognl', 'random-number-ognl', NULL, NULL, '<#assign s=JspTaglibs["/struts-tags"]>
+
+<h3>Hello Struts2 world!</h3>
+<div>This widget is rendered using a FreeMarker script<div> </br>
+
+<div>
+Here is a random number:&nbsp;<@s.property value="number.output" />
+</br>UI language code:&nbsp;<@s.property value="currentLang.code" />
+</br>current user:&nbsp;<@s.property value="uiUser.username" />
+</div>', 1);
+
+INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, locked) VALUES ('random_number_ognl_error', 'random-number-ognl', NULL, NULL, '<pre>
+   __   _ __   _ __   ___   _ __
+ /''__`\/\`''__\/\`''__\/ __`\/\`''__\
+/\  __/\ \ \/ \ \ \//\ \L\ \ \ \/
+\ \____\\ \_\  \ \_\\ \____/\ \_\
+ \/____/ \/_/   \/_/ \/___/  \/_/
+
+</pre>', 1);
